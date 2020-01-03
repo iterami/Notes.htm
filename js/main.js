d689b843813852c9d0d4537c580c1755128f7f4a
Jan Hořava <czechinthemail@gmail.com>
2020-01-02 19:49:26 -0800

    added newline to end of textarea if missing

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
 
 function repo_init(){
     core_repo_init({
+      'events': {
+        'notes': {
+          'onblur': function(){
+              if(this.value.length > 0
+                && !this.value.endsWith('\n')){
+                  this.value += '\n';
+              }
+          },
+        },
+      },
       'info': '<textarea id=notes></textarea>',
       'menu': true,
       'storage': {
