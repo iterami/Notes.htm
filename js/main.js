3660965d0ffa94a66555d750207bffdc25534c1b
Jan Hořava <czechinthemail@gmail.com>
2021-09-25 20:41:01 -0700

    added buttons to scroll to start and end

--- js/main.js
+++ js/main.js
@@ -11,8 +11,31 @@ function repo_init(){
               }
           },
         },
+        'select-end': {
+          'onclick': function(){
+              const element = document.getElementById('notes');
+              const textarea_end = element.value.length;
+              element.focus();
+              element.setSelectionRange(
+                textarea_end,
+                textarea_end
+              );
+              element.scrollLeft = 0;
+              element.scrollTop = element.scrollHeight;
+          },
+        },
+        'select-start': {
+          'onclick': function(){
+              const element = document.getElementById('notes');
+              element.focus();
+              element.setSelectionRange(0, 0);
+              element.scrollLeft = 0;
+              element.scrollTop = 0;
+          },
+        },
       },
-      'info': '<textarea id=notes></textarea>',
+      'info': '<textarea id=notes></textarea><br>'
+        + '<input id=select-start type=button value=Start><input id=select-end type=button value=End>',
       'menu': true,
       'storage': {
         'notes': '',
