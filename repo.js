a6cef69149903eba9ed40cc8eee910a389e3e261
Jan Hořava <czechinthemail@gmail.com>
2025-06-14 01:47:11 -0700

    simplified property usage

--- repo.js
+++ repo.js
@@ -5,20 +5,20 @@ function repo_init(){
       'events': {
         'copy': {
           'onclick': function(){
-              navigator.clipboard.writeText(core_elements['notes'].value);
-              core_elements['notes'].focus();
+              navigator.clipboard.writeText(core_elements.notes.value);
+              core_elements.notes.focus();
           },
         },
         'end': {
           'onclick': function(){
-              core_elements['notes'].focus();
-              const textarea_end = core_elements['notes'].value.length;
-              core_elements['notes'].setSelectionRange(
+              core_elements.notes.focus();
+              const textarea_end = core_elements.notes.value.length;
+              core_elements.notes.setSelectionRange(
                 textarea_end,
                 textarea_end
               );
-              core_elements['notes'].scrollLeft = 0;
-              core_elements['notes'].scrollTop = core_elements['notes'].scrollHeight;
+              core_elements.notes.scrollLeft = 0;
+              core_elements.notes.scrollTop = core_elements.notes.scrollHeight;
           },
         },
         'notes': {
@@ -29,18 +29,18 @@ function repo_init(){
         'sort': {
           'onclick': function(){
               if(globalThis.confirm('Sort all lines?')){
-                  core_elements['notes'].value = core_elements['notes'].value.split('\n').sort().join('\n');
+                  core_elements.notes.value = core_elements.notes.value.split('\n').sort().join('\n');
               }
 
-              core_elements['notes'].focus();
+              core_elements.notes.focus();
           },
         },
         'start': {
           'onclick': function(){
-              core_elements['notes'].focus();
-              core_elements['notes'].setSelectionRange(0, 0);
-              core_elements['notes'].scrollLeft = 0;
-              core_elements['notes'].scrollTop = 0;
+              core_elements.notes.focus();
+              core_elements.notes.setSelectionRange(0, 0);
+              core_elements.notes.scrollLeft = 0;
+              core_elements.notes.scrollTop = 0;
           },
         },
       },
@@ -60,9 +60,9 @@ function repo_init(){
 }
 
 function resize_textarea(){
-    core_elements['notes'].style.height = Math.max(
+    core_elements.notes.style.height = Math.max(
       globalThis.innerHeight - 170,
       50
     ) + 'px';
-    core_elements['notes'].style.width = globalThis.innerWidth + 'px';
+    core_elements.notes.style.width = globalThis.innerWidth + 'px';
 }
