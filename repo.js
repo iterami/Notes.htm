df8b45e086ce1ffd7b05614610ab91914fa20eb8
Jan Hořava <czechinthemail@gmail.com>
2026-05-29 07:20:28 -0700

    removed copy/sort/start/end

--- repo.js
+++ repo.js
@@ -3,24 +3,6 @@
 function repo_init(){
     core_repo_init({
       'events': {
-        'copy': {
-          'onclick': function(){
-              navigator.clipboard.writeText(core_elements.notes.value);
-              core_elements.notes.focus();
-          },
-        },
-        'end': {
-          'onclick': function(){
-              core_elements.notes.focus();
-              const textarea_end = core_elements.notes.value.length;
-              core_elements.notes.setSelectionRange(
-                textarea_end,
-                textarea_end
-              );
-              core_elements.notes.scrollLeft = 0;
-              core_elements.notes.scrollTop = core_elements.notes.scrollHeight;
-          },
-        },
         'notes': {
           'onblur': function(){
               core_storage_save({
@@ -29,29 +11,11 @@ function repo_init(){
               });
           },
         },
-        'sort': {
-          'onclick': function(){
-              if(globalThis.confirm('Sort all lines?')){
-                  core_elements.notes.value = core_elements.notes.value.split('\n').sort().join('\n');
-              }
-
-              core_elements.notes.focus();
-          },
-        },
-        'start': {
-          'onclick': function(){
-              core_elements.notes.focus();
-              core_elements.notes.setSelectionRange(0, 0);
-              core_elements.notes.scrollLeft = 0;
-              core_elements.notes.scrollTop = 0;
-          },
-        },
       },
       'storage': {
         'notes': '',
       },
       'title': 'Notes.htm',
-      'ui': '<button id=copy type=button>Copy</button><button id=sort type=button>Sort</button> <button id=start type=button>Start</button><button id=end type=button>End</button>',
       'ui_elements': [
         'notes',
       ],
